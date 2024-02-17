2ae1e46122e35148dced0fa32d876ce1ca22d6aa
koppulaS <koppulasaicharan@gmail.com>
2024-02-17 17:22:09 +0530

    removing snippets

--- java/Microservices/Microservices/MicroservicesArchitecture-TejaIT/Employee/src/main/java/com/TejaIT/Employee/EmployeeRespository.java
+++
@@ -1,9 +0,0 @@
-package com.TejaIT.Employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRespository extends JpaRepository<Employee, Integer>{
-
-}
--- java/Microservices/Microservices/department/src/main/java/com/TejaIT/department/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.TejaIT.department;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository  extends JpaRepository<Department, Integer>{
-
-}
